cfa755ea8e3b85dfc96efc06474ebad0c27a3f68
black-adm <mattheuscontato17@gmail.com>
2024-01-29 22:33:35 -0300

    first commit

---
+++ estrela-america-v2/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Olá, seja bem vindo</h1>
+    </main>
+  )
+}
